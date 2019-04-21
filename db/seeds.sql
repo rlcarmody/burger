2d66021c0e410974787a4d836d85c32bd4d50adf
RLC <rlcarmody@gmail.com>
2019-04-21 11:46:39 -0700

    Directory structure and seed files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Black and Bleu', false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('The Everything Burger', false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Hawaiian Chicken Burger', false);
